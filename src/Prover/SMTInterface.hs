6aa1b1a2cc82ff8a85baa61f549eea83836fa620
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-04-06 09:24:38 -0700

    now what

--- src/Prover/SMTInterface.hs
+++ src/Prover/SMTInterface.hs
@@ -5,7 +5,7 @@ import Language.Fixpoint.Smt.Interface
 import Language.Fixpoint.Types
 
 makeContext :: FilePath -> [(Symbol, Sort)] -> IO Context 
-makeContext = makeZ3Context
+makeContext = makeZ3Context True
 
 checkValid :: Context -> [Expr] -> Expr -> IO Bool
 checkValid me is q = checkValidWithContext me [] (pAnd is) q
