a3ded8dfa736d298565c9d75036998528f403af5
nikivazou <nikivazou@gmail.com>
2016-05-12 14:02:09 -0700

    pass cfg around

--- src/Prover/SMTInterface.hs
+++ src/Prover/SMTInterface.hs
@@ -2,10 +2,11 @@ module Prover.SMTInterface where
 
 import Language.Fixpoint.Smt.Interface 
 
-import Language.Fixpoint.Types
+import Language.Fixpoint.Types hiding (allowHO)
+import Language.Fixpoint.Types.Config
 
 makeContext :: FilePath -> [(Symbol, Sort)] -> IO Context 
-makeContext = makeZ3Context True
+makeContext = makeZ3Context (defConfig{allowHO = True })
 
 checkValid :: Context -> [Expr] -> Expr -> IO Bool
 checkValid me is q = checkValidWithContext me [] (pAnd is) q
